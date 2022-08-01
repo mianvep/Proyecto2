9049c4d63299688756f9a0b1d8d1a06b1dbf667d
Angel Vera <angelvera.08kaizen@gmail.com>
2022-08-01 12:33:12 -0500

    corrigiendo errores

--- src/models/task.class.js
+++ src/models/task.class.js
@@ -3,13 +3,13 @@ import { LEVELS } from "./levels.enum";
 export class Task {
     name = '';
     description = '';
-    complete = false;
+    completed = false;
     level = LEVELS.NORMAL;
 
     constructor(name, description, completed, level) {
         this.name = name;
         this.description = description;
-        this.complete = completed;
+        this.completed = completed;
         this.level = level;
     };
 }
